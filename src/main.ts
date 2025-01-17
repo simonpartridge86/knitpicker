280c0d0d33a02c29586f4caee55a4717ef3f5acf
simonpartridge86 <simonpartridge86@gmail.com>
2025-01-17 19:24:29 +0000

    updated grid interaction; added clear button

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,12 @@
-const createEmptyGrid = (x: number, y: number): boolean[][] =>
-  Array.from({ length: y }, () => Array(x).fill(false));
+import { renderEmptyGrid } from "./utils";
 
-const emptyGrid = createEmptyGrid(10, 10);
+let defaultWidth = 10;
+let defaultHeight = 10;
 
-const renderGrid = (grid: boolean[][]) => {
-  const gridSize = grid.length;
-  const gridContainer = document.getElementById("grid");
+renderEmptyGrid(defaultWidth, defaultHeight);
 
-  if (gridContainer) {
-    gridContainer.innerHTML = ""; // clears previous grid content
-    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-
-    grid.forEach((row, rowIndex) => {
-      row.forEach((cell, colIndex) => {
-        const gridItem = document.createElement("div");
-        gridItem.classList.add("grid-item", cell.toString()); // Add class based on `true/false`
-        gridItem.textContent = ""; // Optionally, display coordinates or content
-
-        // Add click handler to toggle true/false
-        gridItem.addEventListener("click", () => {
-          grid[rowIndex][colIndex] = !grid[rowIndex][colIndex]; // Toggle value in the array
-          gridItem.classList.toggle("true"); // Update styling dynamically
-          gridItem.classList.toggle("false");
-        });
-
-        gridContainer.appendChild(gridItem);
-      });
-    });
-  }
+const setupClearButton = () => {
+  const clearButton = document.getElementById("clear") as HTMLButtonElement;
+  clearButton.onclick = () => renderEmptyGrid(defaultWidth, defaultHeight);
 };
-
-renderGrid(emptyGrid);
+setupClearButton();
---
+++ src/utils.ts
@@ -0,0 +1,70 @@
+// Creates an empty array x by y dimensions with each element === false
+export const createEmptyGrid = (x: number, y: number): boolean[][] => {
+  return Array.from({ length: y }, () => Array(x).fill(false));
+};
+
+// Creates pixel div element and applies 'pixel' and 'true/false' class
+export const createPixelElement = (cellValue: boolean) => {
+  const pixel = document.createElement("div");
+  pixel.classList.add("pixel", cellValue.toString());
+  return pixel;
+};
+
+// Toggles pixel classes 'true' and 'false'
+export const togglePixel = (pixelElement: HTMLDivElement) => {
+  pixelElement.classList.toggle("true");
+  pixelElement.classList.toggle("false");
+};
+
+// Renders an empty grid with the requested dimensions
+export const renderEmptyGrid = (width: number, length: number) => {
+  let isSelectMode = false;
+  let isDeselectMode = false;
+
+  const grid = createEmptyGrid(width, length);
+  const gridContainer = document.querySelector(".grid") as HTMLDivElement;
+
+  gridContainer.innerHTML = ""; // clears previous grid content
+  gridContainer.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+
+  grid.forEach((row, rowIndex) => {
+    row.forEach((cellValue, colIndex) => {
+      const pixel = createPixelElement(cellValue);
+
+      pixel.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        const pixelValue = grid[rowIndex][colIndex];
+        if (pixelValue === true) {
+          isSelectMode = false;
+          isDeselectMode = true;
+        } else {
+          isSelectMode = true;
+          isDeselectMode = false;
+        }
+        grid[rowIndex][colIndex] = !pixelValue;
+        togglePixel(pixel);
+      });
+
+      pixel.addEventListener("mouseover", (e) => {
+        e.preventDefault();
+        const pixelValue = grid[rowIndex][colIndex];
+        if (isSelectMode && pixelValue === false) {
+          grid[rowIndex][colIndex] = true;
+          togglePixel(pixel);
+        } else if (isDeselectMode && pixelValue === true) {
+          grid[rowIndex][colIndex] = false;
+          togglePixel(pixel);
+        }
+      });
+
+      pixel.addEventListener("mouseup", (e) => {
+        e.preventDefault();
+        isSelectMode = false;
+        isDeselectMode = false;
+        console.table(grid);
+      });
+
+      gridContainer.appendChild(pixel);
+    });
+  });
+};
