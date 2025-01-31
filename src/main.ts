1bf6d16514a3ca687c2be4fe6623d1422381111c
simonpartridge86 <simonpartridge86@gmail.com>
2025-01-31 20:01:06 +0000

    overhauled gridlines to use canvas

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,62 @@
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const gridLines = document.getElementById("grid") as HTMLDivElement;
-const colorInput = document.getElementById("color-input") as HTMLInputElement;
-const toggleGrid = document.getElementById("toggle-grid") as HTMLInputElement;
-const clearButton = document.getElementById(
-  "clear-button"
-) as HTMLButtonElement;
-const drawingContext = canvas.getContext("2d") as CanvasRenderingContext2D;
+// Element selection
+const colorCanvas = document.getElementById("colorCanvas") as HTMLCanvasElement;
+const gridCanvas = document.getElementById("gridCanvas") as HTMLCanvasElement;
+const colorInput = document.getElementById("colorInput") as HTMLInputElement;
+const toggleGrid = document.getElementById("toggleGrid") as HTMLInputElement;
+const clearButton = document.getElementById("clearButton") as HTMLButtonElement;
+const drawingContext = colorCanvas.getContext("2d") as CanvasRenderingContext2D;
+const gridContext = gridCanvas.getContext("2d") as CanvasRenderingContext2D;
 const stitchNumberSelectMenu = document.getElementById(
-  "stitch-number"
+  "stitchNum"
 ) as HTMLSelectElement;
 const rowNumberSelectMenu = document.getElementById(
-  "row-number"
+  "rowNum"
 ) as HTMLSelectElement;
 
+// Constant definitions
 const DEFAULT_STITCH_NUMBER = 10;
 const DEFAULT_ROW_NUMBER = 5;
 const MAX_STITCH_NUMBER = 50;
 const MAX_ROW_NUMBER = 20;
-const pixelWidth = canvas.width / DEFAULT_STITCH_NUMBER;
-const pixelHeight = canvas.height / DEFAULT_ROW_NUMBER;
+const pixelWidth = colorCanvas.width / DEFAULT_STITCH_NUMBER;
+const pixelHeight = colorCanvas.height / DEFAULT_ROW_NUMBER;
 const colorHistory = {} as Record<string, string>;
 
 // Set default color
 colorInput.value = "#009578";
 
-// Initialize the canvas background
+// Initialize color canvas background
 drawingContext.fillStyle = "#ffffff";
-drawingContext.fillRect(0, 0, canvas.width, canvas.height);
+drawingContext.fillRect(0, 0, colorCanvas.width, colorCanvas.height);
 
 // Setup grid lines
-const updateGrid = (stitchNumber: number, rowNumber: number) => {
-  gridLines.style.width = `${canvas.width}px`;
-  gridLines.style.height = `${canvas.height}px`;
-  gridLines.style.gridTemplateColumns = `repeat(${stitchNumber}, 1fr)`;
-  gridLines.style.gridTemplateRows = `repeat(${rowNumber}, 1fr)`;
+function drawGrid(stitchNumber: number, rowNumber: number) {
+  gridContext.clearRect(0, 0, gridCanvas.width, gridCanvas.height);
 
-  [...Array(stitchNumber * rowNumber)].forEach(() =>
-    gridLines.insertAdjacentHTML("beforeend", "<div></div>")
-  );
-};
+  const stitchWidth = gridCanvas.width / stitchNumber;
+  const rowHeight = gridCanvas.height / rowNumber;
 
-updateGrid(DEFAULT_STITCH_NUMBER, DEFAULT_ROW_NUMBER);
+  gridContext.strokeStyle = "#000000";
+  gridContext.lineWidth = 2;
 
-// Setup select menus
+  for (let i = 1; i < stitchNumber; i++) {
+    gridContext.beginPath();
+    gridContext.moveTo(i * stitchWidth, 0);
+    gridContext.lineTo(i * stitchWidth, gridCanvas.height);
+    gridContext.stroke();
+  }
+
+  for (let i = 1; i < rowNumber; i++) {
+    gridContext.beginPath();
+    gridContext.moveTo(0, i * rowHeight);
+    gridContext.lineTo(gridCanvas.width, i * rowHeight);
+    gridContext.stroke();
+  }
+}
+
+drawGrid(DEFAULT_STITCH_NUMBER, DEFAULT_ROW_NUMBER);
+
+// Setup stitch/row select menus
 {
   [...Array(MAX_STITCH_NUMBER)].forEach((_, i) =>
     stitchNumberSelectMenu.insertAdjacentHTML(
@@ -61,13 +75,13 @@ updateGrid(DEFAULT_STITCH_NUMBER, DEFAULT_ROW_NUMBER);
   );
 }
 
-function handleCanvasMousedown(e: MouseEvent) {
+function handleCanvasClick(e: MouseEvent) {
   // Ensure user is using their primary mouse button
   if (e.button !== 0) {
     return;
   }
 
-  const canvasBoundingRect = canvas.getBoundingClientRect();
+  const canvasBoundingRect = colorCanvas.getBoundingClientRect();
   const x = e.clientX - canvasBoundingRect.left;
   const y = e.clientY - canvasBoundingRect.top;
   const cellX = Math.floor(x / pixelWidth);
@@ -83,36 +97,37 @@ function handleCanvasMousedown(e: MouseEvent) {
   }
 }
 
-const handleClearButtonClick = () => {
+function handleClearCanvas() {
   const yes = confirm("Are you sure you wish to clear the canvas?");
 
   if (!yes) return;
 
   drawingContext.fillStyle = "#ffffff";
-  drawingContext.fillRect(0, 0, canvas.width, canvas.height);
-};
+  drawingContext.fillRect(0, 0, colorCanvas.width, colorCanvas.height);
+}
 
-const handleToggleGrid = () => {
-  gridLines.style.display = toggleGrid.checked ? "" : "none";
-};
+function handleToggleGrid(e: Event) {
+  const isChecked = (e.target as HTMLInputElement).checked;
+  gridCanvas.style.display = isChecked ? "" : "none";
+}
 
-const fillCell = (cellX: number, cellY: number) => {
+function fillCell(cellX: number, cellY: number) {
   const startX = cellX * pixelWidth;
   const startY = cellY * pixelHeight;
 
   drawingContext.fillStyle = colorInput.value;
   drawingContext.fillRect(startX, startY, pixelWidth, pixelHeight);
   colorHistory[`${cellX}_${cellY}`] = colorInput.value;
-};
+}
 
-const onDimensionsChange = () => {
+function onDimensionsChange() {
   const currentStitchNumber = parseInt(stitchNumberSelectMenu.value, 10);
   const currentRowNumber = parseInt(rowNumberSelectMenu.value, 10);
-  updateGrid(currentStitchNumber, currentRowNumber);
-};
+  drawGrid(currentStitchNumber, currentRowNumber);
+}
 
-canvas.addEventListener("mousedown", handleCanvasMousedown);
-clearButton.addEventListener("click", handleClearButtonClick);
+colorCanvas.addEventListener("mousedown", handleCanvasClick);
+clearButton.addEventListener("click", handleClearCanvas);
 toggleGrid.addEventListener("change", handleToggleGrid);
 stitchNumberSelectMenu.addEventListener("change", onDimensionsChange);
 rowNumberSelectMenu.addEventListener("change", onDimensionsChange);
